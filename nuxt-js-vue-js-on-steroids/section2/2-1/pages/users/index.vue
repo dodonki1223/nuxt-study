bc4377e3b04048f25d0d900b8fcdbbc8f23f7aa9
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-07 09:27:46 +0900

    :sparkles: 13. From Folders to Routes
    - localhost:3000/usres/ のルーティングの仕組みの理解 （pages フォルダ配下に配置することで自動で作成される）

---
+++ nuxt-js-vue-js-on-steroids/section2/2-1/pages/users/index.vue
@@ -0,0 +1,8 @@
+<!--
+  http://localhost:3000/users/ のルーティングを作成したい時は以下のようなフォルダ構成にする必要あり  
+    - pages/users.vue
+    - pages/users/index.vue
+  -->
+<template>
+  <h1>The users view</h1>
+</template>
