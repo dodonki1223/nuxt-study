ebaa67eb9d7b3f8b8fd3f8efdf106d3925dfcf7d
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-11 08:54:55 +0900

    :sparkles: 課題3: Time to Practice - Fetching Data
    - 管理者用画面でのブログ情報取得ロジックを追加
    - 詳細画面用のデータを取得するロジックを追加

--- nuxt-js-vue-js-on-steroids/blog/pages/admin/_postid/index.vue
+++ nuxt-js-vue-js-on-steroids/blog/pages/admin/_postid/index.vue
@@ -8,21 +8,22 @@
 
 <script>
 import AdminPostForm from '@/components/Admin/AdminPostForm'
+import axios from 'axios'
 
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
   },
-  data() {
-    return {
-      loadedPost: {
-        author: 'Maximilian',
-        title: 'My awesome Post',
-        content: 'Super amazing, thanks for that!',
-        thumbnailLink: 'https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg'
-      }
-    }
+  asyncData(context) {
+    return axios
+      .get('https://sample.firebaseio.com/posts/' + context.params.postId + '.json')
+      .then(res => {
+        return {
+          loadedPost: res.data
+        }
+      })
+      .catch(e => context.error())
   }
 }
 </script>
