d3c3ca53d969980aed58a383fe88da8977d99377
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-10 18:36:52 +0900

    :sparkles: 46. Using Promises in asyncData
    - asyncData を Promises を使用した形に変更する

--- nuxt-js-vue-js-on-steroids/blog/pages/posts/index.vue
+++ nuxt-js-vue-js-on-steroids/blog/pages/posts/index.vue
@@ -11,25 +11,35 @@ export default {
   components: {
     PostList
   },
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: "1",
-            title: "First Post",
-            previewText: "This is our first post!",
-            thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
-          },
-          {
-            id: "2",
-            title: "Second Post",
-            previewText: "This is our second post!",
-            thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
-          }
-        ]
-      });
-    }, 1000);
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
+            }
+          ]
+        });
+      }, 1000);
+      // // エラーを無理やり起こす場合
+      // reject(new Error())
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(e)
+    })
   }
 }
 </script>
