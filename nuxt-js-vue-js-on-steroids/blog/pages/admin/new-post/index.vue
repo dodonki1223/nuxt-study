985309c049af81e4588f826f067ba1685f00d04a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-11 08:39:10 +0900

    :sparkles: 58. Fetching all Posts
    - 更新日付項目を追加
    - 詳細画面用のデータを firebase の firestone から取得する

--- nuxt-js-vue-js-on-steroids/blog/pages/admin/new-post/index.vue
+++ nuxt-js-vue-js-on-steroids/blog/pages/admin/new-post/index.vue
@@ -17,7 +17,11 @@ export default {
   },
   methods: {
     onSubmitted(postData) {
-      axios.post('https://sample.firebaseio.com/posts.json', postData)
+      axios
+        .post('https://sample.firebaseio.com/posts.json', {
+          ...postData, 
+          updatedDate: new Date()
+        })
         .then(result => console.log(result))
         .catch(e => console.log(e))
     }
--- nuxt-js-vue-js-on-steroids/blog/pages/posts/_id/index.vue
+++ nuxt-js-vue-js-on-steroids/blog/pages/posts/_id/index.vue
@@ -15,22 +15,17 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  asyncData(context, callback) {
-    setTimeout(() => {
-      callback(null, {
-        loadedPost: {
-          id: "1",
-          title: "First Post (ID: " + context.route.params.id + ")",
-          previewText: "This is our first post!",
-          author: 'Maximilian',
-          updatedDate: new Date(),
-          content: 'Some dummy text which is definitely not the preview text though!',
-          thumbnail:
-            "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+  asyncData(context) {
+    return axios.get('https://sample.firebaseio.com/posts/' + context.params.id + '.json')
+      .then(res => {
+        return {
+          loadedPost: res.data 
         }
-      });
-    }, 1000);
+      })
+      .catch(e => context.error(e))
   }
 };
 </script>
