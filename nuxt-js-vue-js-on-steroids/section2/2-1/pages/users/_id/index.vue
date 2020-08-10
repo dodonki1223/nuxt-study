6b1678638d66796a4b6b435ec3a6dd21bd4457c1
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-10 09:18:31 +0900

    :sparkles: 16. Validating Parameters
    - ルーティングで渡される id の値を数値でバリデーションチェックを行う

--- nuxt-js-vue-js-on-steroids/section2/2-1/pages/users/_id/index.vue
+++ nuxt-js-vue-js-on-steroids/section2/2-1/pages/users/_id/index.vue
@@ -1,3 +1,13 @@
 <template>
   <h1>A singule user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate({ params }) {
+    // 数値以外の場合は４０４を返すようにする
+    // この辺に書かれている→https://ja.nuxtjs.org/api/pages-validate/
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
