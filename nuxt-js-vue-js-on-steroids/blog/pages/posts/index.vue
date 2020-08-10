84a3d2c5aa788dd804d957c63b956ff5d59ea162
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-10 17:00:58 +0900

    :sparkles: 課題2: Time to Practice - Server-Side Data

--- nuxt-js-vue-js-on-steroids/blog/pages/posts/index.vue
+++ nuxt-js-vue-js-on-steroids/blog/pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -10,6 +10,26 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is our first post!",
+            thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is our second post!",
+            thumbnail: "https://techcrunchjp.files.wordpress.com/2016/05/codecode1.jpg?w=738"
+          }
+        ]
+      });
+    }, 1000);
   }
 }
 </script>
