d7b0ac06820c5d64bd1cbe37ae8f961cacea98c0
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-19 09:10:31 +0900

    :sparkles: 86. A Quick Bugfix
    - サーバーサイドで実行されないように if 文を追加

--- nuxt-js-vue-js-on-steroids/blog/store/index.js
+++ nuxt-js-vue-js-on-steroids/blog/store/index.js
@@ -128,8 +128,12 @@ const createStore = () => {
         vuexContext.commit("clearToken");
         Cookie.remove("jwt");
         Cookie.remove("expirationDate");
-        localStorage.removeItem("token");
-        localStorage.removeItem("tokenExpiration");
+        // サーバーでも実行されてしまうため 「if (process.client)」のコードを追加して
+        // サーバーで実行されないようにする
+        if (process.client) {
+          localStorage.removeItem("token");
+          localStorage.removeItem("tokenExpiration");
+        }
       }
     },
     getters: {
