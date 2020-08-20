a9721eb16c41615a0e3c2392d3ec55e70420c9bf
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2020-08-20 09:52:55 +0900

    :sparkles: 101. Limiting the Amount of http Requests
    - 静的ファイルで作成したものに関しては firebase に対してリクエストを送らなくてもいいような実装にする

--- nuxt-js-vue-js-on-steroids/blog/nuxt.config.js
+++ nuxt-js-vue-js-on-steroids/blog/nuxt.config.js
@@ -95,13 +95,13 @@ module.exports = {
         .then(res => {
           const routes = []
           for (const key in res.data) {
-            routes.push("/posts/" + key)
+            routes.push({ 
+              route: "/posts/" + key,
+              payload: {postData: res.data[key]}
+            });
           }
           return routes
-        })
-      return [
-        "/posts/"
-      ]
+        });
     }
   }
 }
